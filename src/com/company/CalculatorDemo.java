e157cf704e771ddb43520096769aff6377f18b4e
Mikayla Clemons <mikaylaclemons@Mikaylas-MacBook-Air.local>
2019-01-22 15:19:32 -0500

    Create instance of Calculator in CalculatorDemo.

--- src/com/company/CalculatorDemo.java
+++ src/com/company/CalculatorDemo.java
@@ -3,6 +3,9 @@ package com.company;
 public class CalculatorDemo {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Calculator calculator = new Calculator();
+
+        
     }
 }
